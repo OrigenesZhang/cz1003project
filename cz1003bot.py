bb5aad24de6efef9f724d669d6d2a4168c04bf47
Archonsh <Archon@ShenYoulindeMacBook-Pro.local>
2016-09-14 14:21:47 +0800

    fix callbackquery bug and add the prediction featrues

--- cz1003bot.py
+++ cz1003bot.py
@@ -5,27 +5,33 @@ import telepot
 import time
 import math
 import time
-from lib_func import lib_keyboard,lib_keyboard_select,nearest_lib,lib_close,lib_status,welcome_keyboard
-from data_func import html,extract		
+from lib_func import lib_keyboard,lib_keyboard_select,nearest_lib,lib_close,lib_status,welcome_keyboard,lib_p_keyboard,lib_p_keyboard_select
+from data_func import html,extract,ma_A,ma_B	
 bot = telepot.Bot('194233496:AAFTNsUhxGYgb1ij84x0bys3Zhv3CNnE81w')
 
 lib=['LWN Lib','HSS Lib','BIZ Lib','ADM Lib','CHN Lib']
+lib_p=['LWN lib','HSS lib','BIZ lib','ADM lib','CHN lib']
+
 def on_chat_message(msg):
 	if lib_close():
 			return
 	content_type, chat_type, chat_id = telepot.glance(msg)
-	print(content_type)
+	print(content_type, chat_type, chat_id)
 	if content_type=='text':
 		
 		if msg['text']=='Library Current Status Inquiry':
 			bot.sendMessage(chat_id, 'Please Choose Your Library', reply_markup=lib_keyboard())
 
-		# if msg['text']=='predict':
-		#  	bot.sendMessage(chat_id, 'Where do you want to go?', reply_markup=lib_keyboard())
+		elif msg['text']=='Library Status Prediction':
+		  	bot.sendMessage(chat_id, 'Where do you want to go?', reply_markup=lib_p_keyboard())
 
 		elif msg['text'] in lib:
 			n=lib.index(msg['text'])
 			bot.sendMessage(chat_id, 'Please Choose Your Floor and Section', reply_markup=lib_keyboard_select(n))
+
+		elif msg['text'] in lib_p:
+			n=lib_p.index(msg['text'])
+			bot.sendMessage(chat_id, 'Please Choose Your Floor and Section', reply_markup=lib_p_keyboard_select(n))
 
 		else:
 			bot.sendMessage(chat_id, 'Welcome to NTU library assistant,\nwhat can I do for you?', reply_markup=welcome_keyboard())
@@ -40,16 +46,28 @@ def on_callback_query(msg):
 	query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
 	print('Callback Query:', query_id, from_id, query_data)
 	bot.answerCallbackQuery(query_id, text='Got it, just a moment...')
-	if query_data=='lwn':
-		bot.sendMessage(chat_id,lib_status(0))
-	elif query_data=='hss':
-		bot.sendMessage(chat_id,lib_status(0))
-	elif query_data=='biz':
-		bot.sendMessage(chat_id,lib_status(0))
-	elif query_data=='adm':
-		bot.sendMessage(chat_id,lib_status(1))
-	elif query_data=='chn':
-		bot.sendMessage(chat_id,lib_status(1))
+	if len(query_data)==3:
+		if query_data=='lwn':
+			return bot.sendMessage(from_id,text=lib_status(0))
+		elif query_data=='hss':
+			return bot.sendMessage(from_id,text=lib_status(0))
+		elif query_data=='biz':
+			return bot.sendMessage(from_id,text=lib_status(0))
+		elif query_data=='adm':
+			return bot.sendMessage(from_id,text=lib_status(1))
+		elif query_data=='chn':
+			return bot.sendMessage(from_id,text=lib_status(1))
+	else:
+		if query_data=='lwnp':
+			return bot.sendMessage(from_id,text='In the next hour, there may be around %d people in the selected area.' % ma_A(12)) 
+		elif query_data=='hssp':
+			return bot.sendMessage(from_id,text='In the next hour, there may be around %d people in the selected area.' % ma_A(12)) 
+		elif query_data=='bizp':
+			return bot.sendMessage(from_id,text='In the next hour, there may be around %d people in the selected area.' % ma_A(12)) 
+		elif query_data=='admp':
+			return bot.sendMessage(from_id,text='In the next hour, there may be around %d people in the selected area.' % ma_B(12)) 
+		elif query_data=='chnp':
+			return bot.sendMessage(from_id,text='In the next hour, there may be around %d people in the selected area.' % ma_B(12)) 
 
 bot.message_loop({'chat':on_chat_message,
 				  'callback_query':on_callback_query})
--- lib_func.py
+++ lib_func.py
@@ -2,6 +2,7 @@
 # _*_ coding:utf8 _*_
 
 import telepot
+import time
 from telepot.namedtuple import ReplyKeyboardMarkup,KeyboardButton,InlineKeyboardMarkup, InlineKeyboardButton
 from data_func import html,extract,ma_A,ma_B
 def lib_keyboard():
@@ -13,6 +14,18 @@ def lib_keyboard():
 	[KeyboardButton(text='LWN Lib')],[KeyboardButton(text='BIZ Lib')],[KeyboardButton(text='HSS Lib')],[KeyboardButton(text='CHN Lib')],
 
 	[KeyboardButton(text='ADM Lib')]
+	],
+	one_time_keyboard=True
+	)
+	return tmp
+
+def lib_p_keyboard():
+
+	tmp=ReplyKeyboardMarkup(keyboard=
+	[
+	[KeyboardButton(text='LWN lib')],[KeyboardButton(text='BIZ lib')],[KeyboardButton(text='HSS lib')],[KeyboardButton(text='CHN lib')],
+
+	[KeyboardButton(text='ADM lib')]
 	],
 	one_time_keyboard=True
 	)
@@ -52,10 +65,44 @@ def lib_keyboard_select(n):
 	tmp=[lwn_lib_keyboard,hss_lib_keyboard,biz_lib_keyboard,adm_lib_keyboard,chn_lib_keyboard]
 	return tmp[n]
 
-def welcome_keyboard()
+def lib_p_keyboard_select(n):
+	
+	lwn_lib_keyboard=InlineKeyboardMarkup(inline_keyboard=
+		[
+		[InlineKeyboardButton(text='5F Quite Zone Sect A',callback_data='lwnp')],[InlineKeyboardButton(text='5F Quite Zone Sect B',callback_data='lwnp')],
+		[InlineKeyboardButton(text='5F Quite Zone Sect C',callback_data='lwnp')],[InlineKeyboardButton(text='4F',callback_data='lwnp')],
+		[InlineKeyboardButton(text='3F',callback_data='lwnp')],[InlineKeyboardButton(text='2F',callback_data='lwnp')],
+		]
+		)
+	hss_lib_keyboard=InlineKeyboardMarkup(inline_keyboard=
+		[
+		[InlineKeyboardButton(text='Inside',callback_data='hssp')],[InlineKeyboardButton(text='Outside',callback_data='hssp')],
+		]
+		)
+	biz_lib_keyboard=InlineKeyboardMarkup(inline_keyboard=
+		[
+		[InlineKeyboardButton(text='B4 Quiet Zone',callback_data='bizp')],[InlineKeyboardButton(text='B3',callback_data='bizp')],
+		[InlineKeyboardButton(text='B2 & B1',callback_data='bizp')],
+		]
+		)
+	adm_lib_keyboard=InlineKeyboardMarkup(inline_keyboard=
+		[
+		[InlineKeyboardButton(text='Section A',callback_data='admp')],[InlineKeyboardButton(text='Section B',callback_data='admp')],
+		]
+		)
+	chn_lib_keyboard=InlineKeyboardMarkup(inline_keyboard=
+		[
+		[InlineKeyboardButton(text='Section A',callback_data='chnp')],[InlineKeyboardButton(text='Section B',callback_data='chnp')],
+		[InlineKeyboardButton(text='Section C',callback_data='chnp')],
+		]
+		)
+	tmp=[lwn_lib_keyboard,hss_lib_keyboard,biz_lib_keyboard,adm_lib_keyboard,chn_lib_keyboard]
+	return tmp[n]	
+
+def welcome_keyboard():
 	tmp=ReplyKeyboardMarkup(keyboard=
 	[
-	[KeyboardButton(text='Library Current Status Inquiry'],
+	[KeyboardButton(text='Library Current Status Inquiry')],
 
 	[KeyboardButton(text='Library Status Prediction')]
 	],
@@ -94,7 +141,8 @@ def lib_close():
 			return 1
 	elif t.tm_hour >= 21:
 			bot.sendMessage(chat_id,"Oops, libraries have closed.")
-			return 1	
+			return 1
+	return 0	
 	
 
 def lib_status(lib_name):
@@ -102,8 +150,7 @@ def lib_status(lib_name):
 	num = html()[lib_name]
 	empty = seats[lib_name]-num
 	if empty < 10:
-		return 'There are only 'str(empty)+' seats available, quite crowded...'
+		return 'There are only '+str(empty)+' seats available, quite crowded...'
 	else:
-		return 'There are 'str(empty)," seats available, lots of space!"
-
+		return 'There are '+str(empty)+" seats available, lots of space!"
 
